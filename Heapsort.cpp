7e33a52fc2bbc558268b2a34962eb1550b64400c
Exaynts <exayntszhenya@gmail.com>
2025-03-25 16:27:50 +0300

    fix and rename Heapsort to Heapsort.cpp

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -13,8 +13,8 @@ void Tree(vector <int> A, int n) {
         }
     }
     // Для i = 2*i + 2
-    int i = 0;
-    while (i < ((n-2)/2) {
+    i = 0;
+    while (i < ((n-2)/2)) {
         if (A[i] > A[2*i+2]) {
             swap(A[i], A[2*i+2]);
             i = i*2+2;
@@ -26,16 +26,16 @@ int main() {
 // Определение массива
 int n = 8;
 vector <int> A(n);
-A(n) = {1, 10, 13, 15, 8, 2, 3, 4};
+A = {1, 10, 13, 15, 8, 2, 3, 4};
 // Этап 1.
 for(int j = 0; j < n; j++) {
     Tree(A, n); }
 // Этап 2.
-for(int j = 0; j < n; j++) {
+for(int j = 1; j < n; j++) {
     swap(A[0], A[n-j]);
     Tree(A, n); }
 // Вывод массива
-for(int i = 0; i < n; i++)
-    cout << A[i] << " ";
+for(int i = 0; i < n; i++) {
+    cout << A[i] << " "; }
 return 0;
 }
